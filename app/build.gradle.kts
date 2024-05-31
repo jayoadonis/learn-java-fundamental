0083b1a24cf7670d003cbc41cae09f81045edf9e
jayoadonis <jayo.adonisraphael@gmail.com>
2024-05-31 18:55:18 +0800

    init: 0.0.1, learn-java-fundamental

---
+++ app/build.gradle.kts
@@ -0,0 +1,19 @@
+
+
+plugins {
+    java
+    application
+}
+
+project.version = "0.0.1"
+project.group = "jayo.arb.learn_j"
+
+val ROOT_PROJECT_NAME = project.rootProject.name
+    .replace("-", "_")
+
+project.application {
+
+    this.mainClass.set(
+        "${project.group}.${ROOT_PROJECT_NAME}.${project.name}.MainExe"
+    );
+}
---
+++ app/src/main/jayo/arb/learn_j/learn_java_fundamental/app/MainExe.java
@@ -0,0 +1,8 @@
+package jayo.arb.learn_j.learn_java_fundamental.app;
+
+public class MainExe {
+
+    public static void main(String[] args) {
+        System.out.println("Hi there from: learn-java-fundamental");
+    }
+}
